2ebb3979aea287ebcb96ed914acb25ae521c5f2c
kmddr <86179894+kmddr@users.noreply.github.com>
2021-07-23 14:57:41 -0400

    Update covers.js

--- bin/covers.js
+++ bin/covers.js
@@ -3,24 +3,25 @@
 const ART_WIDTH = 1400;
 const ART_HEIGHT = 1400;
 
-// get stuff
+// get stuff (wow thanks ethan this comment is very useful)
 const fs = require("fs");
 const canvas = require("canvas");
 
 // assets will be where the actual assets get stored (e.g. assets.statues.Helios.png), paths will be the config for the different objects
-let assets = {};
+let assets = {
+  floor: "../images/floor.png", // Checkerboard floor
+};
 let paths = {
-  floors:     "./images/floors/", // the checkerboard floor
-  paintings:  "./images/paintings/", // the painting under the text
-  statues:    "./images/statues/", // the helios statue
-  etc:        "./images/etc/", // the random bullshit that gives the cover art some more PAZAZ
+  paintings:  "../images/paintings/", // the painting under the text
+  statues:    "../images/statues/", // the helios statue
+  etc:        "../images/etc/", // the random bullshit that gives the cover art some more PAZAZ
 };
 
 // canvas bullcrap go here lol
 let coverArtCanvas = canvas.createCanvas(ART_WIDTH,ART_HEIGHT);
 let coverArtContext = coverArtCanvas.getContext("2d");
 
-console.log(fs.readdir("./images/"));
+console.log(fs.readdir("../images/"));
 
 Object.keys(paths).forEach(currentAssetType => {
   let currentPath = paths[currentAssetType];
